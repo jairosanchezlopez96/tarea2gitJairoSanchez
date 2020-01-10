1dfcb69def0d8e5633435802dc21354dd2480a7f
jairo <jairo9slopez@gmail.com>
2020-01-10 14:19:28 +0100

    Añade getters y setters 2

--- src/tarea2gitjairosanchez/Jugador.java
+++ src/tarea2gitjairosanchez/Jugador.java
@@ -13,6 +13,30 @@ public class Jugador {
     String nombre;
     int votos;
     int id;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
     
     public Jugador( String nombre, int id, int votos){
     this.nombre = nombre;
