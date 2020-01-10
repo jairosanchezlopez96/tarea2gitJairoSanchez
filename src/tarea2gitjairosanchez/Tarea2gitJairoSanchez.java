a2027b26cb0d6be755b1292b2b450ac2bdc49579
jairo <jairo9slopez@gmail.com>
2020-01-10 13:58:01 +0100

    Añade jugador

--- src/tarea2gitjairosanchez/Tarea2gitJairoSanchez.java
+++ src/tarea2gitjairosanchez/Tarea2gitJairoSanchez.java
@@ -15,7 +15,7 @@ public class Tarea2gitJairoSanchez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Jugador j1 = new Jugador("js",1,1);
     }
     
 }
